2051c9c0ac5c141bbe64396514e190051a6aedce
anajmbf <ajmbf1@aluno.ifal.edu.br>
2022-09-26 18:27:43 +0000

    subtracaoTeste

---
+++ api/src/test/java/app/subtracaoTeste.java
@@ -0,0 +1,24 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class subtracaoTeste {
+    
+    @Test
+        public void testandoBase(){
+            Subtracao subtracao = new Subtracao();
+            double resultado = subtracao.calcula("9","2");
+            assertEquals(7.0, resultado);
+        }
+
+        @Test
+        public void testandoBase2(){
+            Subtracao subtracao = new Subtracao();
+            double resultado = subtracao.calcula("8","1");
+            assertEquals(7.0, resultado);
+        }
+
+
+}

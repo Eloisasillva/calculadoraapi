3d0f899bec86005edd2b1badeaab1864798ea9b6
anajmbf <ajmbf1@aluno.ifal.edu.br>
2022-09-26 17:50:59 +0000

    .

--- api/src/main/java/app/App.java
+++ api/src/main/java/app/App.java
@@ -8,15 +8,10 @@ public class App extends Jooby {
     mvc(new Index());
     mvc(new Adicao());
     mvc(new Divisao());
-
-}
-
     mvc(new Subtracao());
-  }
-
-
+}
   public static void main(final String[] args) {
     runApp(args, App::new);
   }
 
-}
+ }
